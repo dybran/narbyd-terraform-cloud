c2e700cc1d5a6af54faad591d0ac44548a250def
solomon onwuasoanya <onwuasoanyasc@gmail.com>
2023-06-02 20:47:04 +0100

    update

--- terraform/modules/ASG/asg-wordpress-tooling.tf
+++ terraform/modules/ASG/asg-wordpress-tooling.tf
@@ -22,11 +22,11 @@ resource "aws_autoscaling_group" "wordpress-asg" {
 }
 
 
-# attaching autoscaling group of  wordpress application to internal loadbalancer
-resource "aws_autoscaling_attachment" "asg_attachment_wordpress" {
-  autoscaling_group_name = aws_autoscaling_group.wordpress-asg.id
-  lb_target_group_arn   = var.wordpress-alb-tgt
-}
+# # attaching autoscaling group of  wordpress application to internal loadbalancer
+# resource "aws_autoscaling_attachment" "asg_attachment_wordpress" {
+#   autoscaling_group_name = aws_autoscaling_group.wordpress-asg.id
+#   lb_target_group_arn   = var.wordpress-alb-tgt
+# }
 
 
 
@@ -53,8 +53,8 @@ resource "aws_autoscaling_group" "tooling-asg" {
   }
 }
 
-# # # attaching autoscaling group of  tooling application to internal loadbalancer
-resource "aws_autoscaling_attachment" "asg_attachment_tooling" {
-  autoscaling_group_name = aws_autoscaling_group.tooling-asg.id
-  lb_target_group_arn   = var.tooling-alb-tgt
-}
+# # # # attaching autoscaling group of  tooling application to internal loadbalancer
+# resource "aws_autoscaling_attachment" "asg_attachment_tooling" {
+#   autoscaling_group_name = aws_autoscaling_group.tooling-asg.id
+#   lb_target_group_arn   = var.tooling-alb-tgt
+# }
