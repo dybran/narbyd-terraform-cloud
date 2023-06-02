3fe3526676b161c5066beee51040beb712c051c0
solomon onwuasoanya <onwuasoanyasc@gmail.com>
2023-06-02 19:49:18 +0100

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,32 +1,32 @@
 #############################
 ##creating bucket for s3 backend
 #########################
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "narbyd-dev-terraform-bucket"
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket = "narbyd-dev-terraform-bucket"
 
-  versioning {
-    enabled = true
-  }
-  force_destroy = true
+#   versioning {
+#     enabled = true
+#   }
+#   force_destroy = true
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
+#   server_side_encryption_configuration {
+#     rule {
+#       apply_server_side_encryption_by_default {
+#         sse_algorithm = "AES256"
+#       }
+#     }
+#   }
+# }
 
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "narbyd-terraform-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+# resource "aws_dynamodb_table" "terraform_locks" {
+#   name         = "narbyd-terraform-locks"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "LockID"
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+# }
 
 # creating VPC
 module "VPC" {
